37ef7c4649d64c3a2936ff96690729b405c06fe6
silvino.albano <silvino.albano@cfp.gov.tl>
2024-02-07 08:40:09 +0900

    Create Input_data_husi_user
    Python input data from user husi type data sira

---
+++ 00. Template/input_data_husi_user.py
@@ -0,0 +1,17 @@
+#Episode input user
+#data nebe ita input iha ne sempre string
+data = input ("Input Data/Naran :")
+print("data = ",data, ", type=",type(data))
+
+#se ita hakarak foti integer mak:
+data_int= int(input("Input Numeru/Tinan: "))
+print("data = ", data_int,",type=", type(data_int))
+
+# angka =float(input("masukan angka: "))
+# angka =int(input("masukan angka: "))
+# print("data = ", angka,",type=", type(angka))
+
+#oinsa ho boolean
+biner= bool(int(input("Input Valor Boolean: ")))
+
+print ("data = ",biner,",type= ", type(biner))
