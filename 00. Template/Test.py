713c67eb5d4bf4d5f2f98fe510f378055a6c22db
silvino.albano <silvino.albano@cfp.gov.tl>
2024-02-07 07:53:03 +0900

    Create File Test
    Teste pritn iha python

---
+++ 00. Template/Test.py
@@ -0,0 +1,46 @@
+#2.
+#import time
+#start_time = time.time()
+
+#1.maneira servisu program no bytecode
+#print("hello heello diak kle?")
+#print("hello")
+#print("world")
+
+#print ("hello world")
+
+#ida nee coment
+
+#exemplu comment multiline
+""""ida nee mak komen multiline
+   teste teste"""
+#a=10
+#print (a)
+
+#print (time.time()- start_time, "detik")
+#ita bele compile ho bytecode ho maneira:cd 
+#1.python -m py_compile Main.py
+
+# variable teampat menyinpan nilai
+a=10
+x=4
+panjang=1000
+
+#pangilan pertama
+print ("Valor a=", a)
+print("Nilai x =", x)
+print("Valor Panjang=", panjang)
+
+#penamaan
+valor_y=15 #uza underscore
+juta10=10 #numeru iha kotuk bele
+valorZ=17.5 #hnsan ne mos bele
+
+#pangilan kedua
+print("Valor a=", a)
+a=7
+print ("valor a=", a)
+
+#assignment inderect
+b=a
+print("Valor b=", b)
