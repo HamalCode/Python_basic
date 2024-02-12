76e50a7f92d850210af7e7b2699f5a9bf9a00c24
silvino.albano <silvino.albano@cfp.gov.tl>
2024-02-13 08:09:51 +0900

    Manupulasi string and operator_assignment

---
+++ 00. Template/manupula_string.py
@@ -0,0 +1,5 @@
+# operator dala bentuk method
+
+# merubah case dari string
+
+#merubah semua ke upper case
---
+++ 00. Template/operator_assingment.py
@@ -0,0 +1,28 @@
+# operasi ditambahkan dengan assignment
+a = 5 
+print ("valor a=", a)
+
+a += 1 # signifika a= a+1
+print ("valor a+=, valor a mak:",a)
+
+a -= 2 # signifika a= a-2
+print ("valor a-=2, valor a mak:",a)
+
+a *= 5 # signifika a= a*5
+print ("valor a*=2, valor a mak:",a)
+
+a /= 2 # signifika a= a/2
+print ("valor a/=2, valor a mak:",a)
+
+#modulus
+b = 10
+print("\nvalor b =",b)
+
+b %= 3
+print("valor b %=3,  maka resultadu valor b ",b)
+
+b = 10
+print("\nvalor b =",b)
+
+b //= 3
+print("valor b //=3,  maka resultadu valor b ",b)
