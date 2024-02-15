9a4a5d80201dfb26243729de57bf938b04e35a41
Silvino Albano <97967209+HamalCode@users.noreply.github.com>
2024-02-15 12:20:01 +0900

    ELIF python

---
+++ 00. Template/elif.py
@@ -0,0 +1,13 @@
+# ELIF
+naran = input("Ita Naran saida? ") #kondisaun 1
+if naran == "apeu":
+    print("haii broo dkl!") #asaun true 1
+    
+elif naran == "Tole": #kondisaun 2
+    print("aii daske u ga") #asaun true 2
+else:
+    print("aii jus la koinese u ida") #asaun false
+    
+print(" remata programa")
+    
+
